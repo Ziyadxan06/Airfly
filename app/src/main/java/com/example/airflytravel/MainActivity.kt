d08547655301f7e44b1f99e6fe8453ef2c36a60d
Ziyadxan06 <ziyadxan.dovletov06@gmail.com>
2026-01-22 15:08:28 +0400

    Setup Android 12 splash screen

--- app/src/main/java/com/example/airflytravel/MainActivity.kt
+++ app/src/main/java/com/example/airflytravel/MainActivity.kt
@@ -1,12 +1,14 @@
 package com.example.airflytravel
 
 import android.os.Bundle
+import androidx.activity.ComponentActivity
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.FragmentActivity
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : FragmentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
