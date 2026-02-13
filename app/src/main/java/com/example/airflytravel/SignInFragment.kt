1f6f3805d84cd214a436136741cb103e98f853df
Ziyadxan06 <ziyadxan.dovletov06@gmail.com>
2026-02-13 16:36:09 +0400

    feat: add SignInFragment with ViewBinding support

---
+++ app/src/main/java/com/example/airflytravel/SignInFragment.kt
@@ -0,0 +1,33 @@
+package com.example.airflytravel
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.airflytravel.databinding.FragmentSignInBinding
+import com.example.airflytravel.databinding.FragmentSignUpBinding
+
+class SignInFragment : Fragment() {
+
+    private var _binding: FragmentSignInBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        _binding = FragmentSignInBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
