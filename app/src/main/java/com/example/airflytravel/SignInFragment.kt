f3e69c07732da28b4691fba403fa5a66f4e2f760
Ziyadxan06 <ziyadxan.dovletov06@gmail.com>
2026-02-16 15:04:36 +0400

    feat(ui): complete Sign In fragment layout

--- app/src/main/java/com/example/airflytravel/SignInFragment.kt
+++ app/src/main/java/com/example/airflytravel/SignInFragment.kt
@@ -1,10 +1,17 @@
 package com.example.airflytravel
 
+import android.graphics.Color
 import android.os.Bundle
+import android.text.SpannableString
+import android.text.Spanned
+import android.text.TextPaint
+import android.text.method.LinkMovementMethod
+import android.text.style.ClickableSpan
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.example.airflytravel.databinding.FragmentSignInBinding
 import com.example.airflytravel.databinding.FragmentSignUpBinding
 
@@ -24,6 +31,39 @@ class SignInFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        val tvExistsAccount = binding.accountExistsMessageSignIn
+
+        val tvText = "Don't have an account? Sign Up"
+
+        val tvSpannable = SpannableString(tvText)
+
+        val accountStart = tvText.indexOf("Sign Up")
+        val accountEnd = accountStart + "Sign Up".length
+
+        val tvClikableSpan = object : ClickableSpan(){
+            override fun onClick(p0: View) {
+                findNavController().navigate(R.id.action_signInFragment_to_signUpFragment )
+            }
+
+            override fun updateDrawState(ds: TextPaint) {
+                super.updateDrawState(ds)
+                ds.color = Color.parseColor("#207DFF")
+                ds.isFakeBoldText = true
+                ds.isUnderlineText = true
+            }
+        }
+
+        tvSpannable.setSpan(
+            tvClikableSpan,
+            accountStart,
+            accountEnd,
+            Spanned.SPAN_EXCLUSIVE_EXCLUSIVE
+        )
+
+        tvExistsAccount.text = tvSpannable
+        tvExistsAccount.movementMethod = LinkMovementMethod.getInstance()
+        tvExistsAccount.highlightColor = Color.TRANSPARENT
     }
 
     override fun onDestroyView() {
