e70e481e375f28edd59d5a948493bbbcb9f4dcba
Ziyadxan06 <ziyadxan.dovletov06@gmail.com>
2026-02-17 16:31:49 +0400

    feat: add SuccessDialogFragment for reusable success notifications

---
+++ app/src/main/java/com/example/airflytravel/SuccessDialogFragment.kt
@@ -0,0 +1,38 @@
+package com.example.airflytravel
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.DialogFragment
+import com.example.airflytravel.databinding.FragmentSuccessDialogBinding
+
+class SuccessDialogFragment : DialogFragment() {
+    private var _binding: FragmentSuccessDialogBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setStyle(STYLE_NORMAL, R.style.RoundedCornersDialog)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        _binding = FragmentSuccessDialogBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+
+        _binding = null
+    }
+}
