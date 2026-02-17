e6a396984c41c614a4ed539da8e21892924a918b
Ziyadxan06 <ziyadxan.dovletov06@gmail.com>
2026-02-17 13:23:53 +0400

    feat(ui): implement OTP input fields with rounded corners and focus effects

--- app/src/main/java/com/example/airflytravel/OtpVerificationFragment.kt
+++ app/src/main/java/com/example/airflytravel/OtpVerificationFragment.kt
@@ -1,10 +1,14 @@
 package com.example.airflytravel
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.KeyEvent
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
 import com.example.airflytravel.databinding.FragmentEmailSentBinding
 import com.example.airflytravel.databinding.FragmentOtpVerificationBinding
 
@@ -23,6 +27,37 @@ class OtpVerificationFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        setupOtp()
+    }
+
+    private fun setupOtp() {
+        val edits = listOf<EditText>(binding.et1, binding.et2, binding.et3, binding.et4, binding.et5, binding.et6)
+
+        edits.forEachIndexed { index, editText ->
+            editText.addTextChangedListener(object : TextWatcher{
+                override fun afterTextChanged(p0: Editable?) {
+                    if(p0?.length == 1 && index < edits.size - 1){
+                        edits[index + 1].requestFocus()
+                    }
+                }
+
+                override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+
+                override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+            })
+
+            editText.setOnKeyListener { _, keyCode, event ->
+                if (keyCode == KeyEvent.KEYCODE_DEL &&
+                    event.action == KeyEvent.ACTION_DOWN &&
+                    editText.text.isEmpty() &&
+                    index > 0
+                ) {
+                    edits[index - 1].requestFocus()
+                }
+                false
+            }
+        }
     }
 
     override fun onDestroyView() {
